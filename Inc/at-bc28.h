bcea89dfa871f51ac7dfadcfbb52a889e8011a33
yllluuu <163637849+yllluuu@users.noreply.github.com>
2024-07-29 13:04:21 +0800

    Update at-bc28.h

--- Inc/at-bc28.h
+++ Inc/at-bc28.h
@@ -8,6 +8,8 @@
 #ifndef INC_AT_BC28_H_
 #define INC_AT_BC28_H_
 
+#include "at_cmd.h"
+
 enum
 {
 	STAT_INIT,
@@ -21,15 +23,32 @@ typedef struct NBiot_conf_s
 	int		status;
 	char	manufacturers[64];
 	char	model[64];
-	char	IMEI[64];
+	char	imei[64];
 	char	SIM[64];
-	char	CSQ[64];
+	char	csq[64];
 }NBiot_conf_t;
+
+#define NCDP			"221.229.214.202,5683"
 
 extern NBiot_conf_t		NBconf;
 
-extern int NB_RSET_OK();
-extern int NB_HDW_OK();
-extern int NB_CONF_OK();
-
+extern int nb_reset_ok(comport_t *comport);
+extern int nb_hdw_ok(comport_t *comport);
+extern int nb_conf_ok(comport_t *comport);
+extern int bc28_Check_AT(comport_t *comport);
+extern int bc28_Reset(comport_t *comport);
+extern int bc28_Get_MANUF(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Get_Module(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Check_IMEI(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Check_SIMCD(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Set_AUTOCNT(comport_t *comport);
+extern int bc28_Check_CFUN(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Check_CSQ(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Set_Attach_Net(comport_t *comport);
+extern int bc28_Check_Attach_Net(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Check_Reg_Status(comport_t *comport, char *reply_buf,size_t size);
+extern int bc28_Check_IP(comport_t *comport, char *reply_buf,size_t size);
+extern int bc28_Set_IP_PORT(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Check_IP_PORT(comport_t *comport, char *reply_buf, size_t size);
+extern int bc28_Check_IOT(comport_t *comport, char *reply_buf, size_t size);
 #endif /* INC_AT_BC28_H_ */
