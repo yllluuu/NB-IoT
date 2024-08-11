d46c5339cc7e9d6ae83381191febe27e52dd4ab4
yllluuu <163637849+yllluuu@users.noreply.github.com>
2024-08-11 15:15:48 +0800

    优化代码

--- src/at_cmd.c
+++ src/at_cmd.c
@@ -1,12 +1,12 @@
 /*********************************************************************************
- *      Copyright:  (C) 2024 niuchunmin
+ *      Copyright:  (C) 2024 yanglu
  *                  All rights reserved.
  *
  *       Filename:  at_cmd.c
  *    Description:  This file 
  *                 
  *        Version:  1.0.0(16/07/24)
- *         Author:  Niu Chunmin <2430815397@qq.com>
+ *         Author:  yanglu <yanglu@2012174595@qq.com>
  *      ChangeLog:  1, Release initial version on "16/07/24 20:21:26"
  *                 
  ********************************************************************************/
